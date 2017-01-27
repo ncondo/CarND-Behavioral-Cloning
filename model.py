636e4b4110d582ca7ec284dc8f6a6f5836d2f066
Nick <nicholas.condo@gmail.com>
2017-01-26 23:57:46 -0500

    Load training data from log file

--- model.py
+++ model.py
@@ -1,1 +1,29 @@
-# Create and train model here
+import pickle
+import tensorflow as tf
+import numpy as np
+import cv2
+import csv
+
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+
+def load_data():
+    """
+    Utility function to load training data from driving_log.csv file.
+    """
+    training_file = 'data/driving_log.csv'
+
+    with open(training_file, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    images = []
+    steering_angle = []
+    for line in data:
+        images.append(line[0])
+        steering_angle.append(line[3])
+        images.append(line[1])
+        steering_angle.append(line[3])
+        images.append(line[2])
+        steering_angle.append(line[3])
+
