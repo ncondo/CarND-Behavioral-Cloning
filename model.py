49d050140118d733af43aecc3f6ba50cb50518f5
Nick <nicholas.condo@gmail.com>
2017-01-30 22:25:25 -0500

    Remove samples_per_epoch parameter from generator

--- model.py
+++ model.py
@@ -48,20 +48,19 @@ def get_csv_data(training_file):
     return image_names, steering_angles
 
 
-def generate_batch_2(X_train, y_train, samples_per_epoch, batch_size=64):
+def generate_batch_2(X_train, y_train, batch_size=64):
     image_names = X_train
     steering_angles = y_train
     while 1:
         images = np.zeros((batch_size, 160, 320, 3), dtype=np.float32)
         angles = np.zeros((batch_size,), dtype=np.float32)
-        for i in range(0, samples_per_epoch, batch_size):
-            for j in range(batch_size):
-                image = Image.open('data/' + image_names[i+j])
-                image = np.array(image, dtype=np.float32)
-                image = random_brightness(image)
-                images[j] = image
-                angles[j] = steering_angles[i+j]
-            yield images, angles
+        for i in range(batch_size):
+            image = Image.open('data/' + image_names[i])
+            image = np.array(image, dtype=np.float32)
+            image = random_brightness(image)
+            images[i] = image
+            angles[i] = steering_angles[i]
+        yield images, angles
 
 
 
@@ -155,8 +154,7 @@ if __name__=="__main__":
     X_train, y_train = get_csv_data(training_file)
 
     model = get_model()
-    samples_per_epoch = 28426
-    model.fit_generator(generate_batch_2(X_train, y_train, samples_per_epoch), samples_per_epoch=28426, nb_epoch=40)#, validation_data=generate_batch_2(X_train, y_train), nb_val_samples=1024)
+    model.fit_generator(generate_batch_2(X_train, y_train), samples_per_epoch=28426, nb_epoch=40)#, validation_data=generate_batch_2(X_train, y_train), nb_val_samples=1024)
 
     print('Saving model weights and configuration file.')
 
