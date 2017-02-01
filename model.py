0567b40a876f0994474766ea5ed7c5999a31e601
Nick <nicholas.condo@gmail.com>
2017-02-01 02:07:47 -0500

    Remove l2 regularization and use dropout after conv layers

--- model.py
+++ model.py
@@ -140,29 +140,39 @@ def get_model():
         Lambda(normalize, input_shape=(66, 200, 3)),
         # Convolutional layer 1 24@31x98 | 5x5 kernel | 2x2 stride | relu activation 
         Convolution2D(24, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+        # Dropout with drop probability of .2 (keep probability of .8)
+        Dropout(.2),
         # Convolutional layer 2 36@14x47 | 5x5 kernel | 2x2 stride | relu activation
         Convolution2D(36, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+        # Dropout with drop probability of .2 (keep probability of .8)
+        Dropout(.2),
         # Convolutional layer 3 48@5x22  | 5x5 kernel | 2x2 stride | relu activation
         Convolution2D(48, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='he_normal'),
+        # Dropout with drop probability of .2 (keep probability of .8)
+        Dropout(.2),
         # Convolutional layer 4 64@3x20  | 3x3 kernel | 1x1 stride | relu activation
         Convolution2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal'),
+        # Dropout with drop probability of .2 (keep probability of .8)
+        Dropout(.2),
         # Convolutional layer 5 64@1x18  | 3x3 kernel | 1x1 stride | relu activation
         Convolution2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='he_normal'),
+        # Dropout with drop probability of .2 (keep probability of .8)
+        Dropout(.2),
         # Flatten
         Flatten(),
-        # Dropout with keep probability of .2
+        # Dropout with drop probability of .2 (keep probability of .8)
         Dropout(.2),
         # Fully-connected layer 1 | 100 neurons
-        Dense(100, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with keep probability of .5
+        Dense(100, activation='elu', init='he_normal'),# W_regularizer=l2(0.001)),
+        # Dropout with drop probability of .5
         Dropout(.5),
         # Fully-connected layer 2 | 50 neurons
-        Dense(50, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with keep probability of .5
+        Dense(50, activation='elu', init='he_normal'),# W_regularizer=l2(0.001)),
+        # Dropout with drop probability of .5
         Dropout(.5),
         # Fully-connected layer 3 | 10 neurons
-        Dense(10, activation='elu', init='he_normal', W_regularizer=l2(0.001)),
-        # Dropout with keep probability of .5
+        Dense(10, activation='elu', init='he_normal'),# W_regularizer=l2(0.001)),
+        # Dropout with drop probability of .5
         Dropout(.5),
         # Output
         Dense(1, init='he_normal')
